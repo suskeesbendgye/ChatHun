a3accda5130262223a20eec48f5250994552e6b5
suskeesbendgye <110496279+suskeesbendgye@users.noreply.github.com>
2022-08-07 18:06:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,37 +9,37 @@
 <body>
   <div class="wrapper">
     <section class="form signup">
-      <header>Jordan's Live Chat Application</header>
+      <header>Chat App</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="name-details">
           <div class="field input">
-            <label>First Name</label>
-            <input type="text" name="fname" placeholder="First name" required>
+            <label>Keresztnév</label>
+            <input type="text" name="fname" placeholder="Keresztnév" required>
           </div>
           <div class="field input">
-            <label>Last Name</label>
-            <input type="text" name="lname" placeholder="Last name" required>
+            <label>Vezetéknév</label>
+            <input type="text" name="lname" placeholder="Vezetéknév" required>
           </div>
         </div>
         <div class="field input">
-          <label>Email Address</label>
-          <input type="text" name="email" placeholder="Enter your email" required>
+          <label>Email cím</label>
+          <input type="text" name="email" placeholder="Írd be az e-mail címed" required>
         </div>
         <div class="field input">
-          <label>Password</label>
-          <input type="password" name="password" placeholder="Enter new password" required>
+          <label>Jelszó</label>
+          <input type="password" name="password" placeholder="Írd be a jelszavad" required>
           <i class="fas fa-eye"></i>
         </div>
         <div class="field image">
-          <label>Select Image</label>
+          <label>Válassz Képet</label>
           <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
         </div>
         <div class="field button">
-          <input type="submit" name="submit" value="Continue to Chat">
+          <input type="submit" name="submit" value="Tovább a Chatre">
         </div>
       </form>
-      <div class="link">Already signed up? <a href="login.php">Login now</a></div>
+      <div class="link">Már regisztráltál? <a href="login.php">Lépj be</a></div>
     </section>
   </div>
 
